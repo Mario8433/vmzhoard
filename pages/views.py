04f553da25924aa21cb885076bd9dee38c98f7ed
Mario8433 <mario84.233@gmail.com>
2024-09-22 23:34:17 -0600

    Change the package reference of texts-models of views-py to relative

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
-from text.models import Text
+from ..text.models import Text
 from .forms import *
 
 import re
